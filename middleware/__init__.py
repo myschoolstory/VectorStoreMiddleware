3836dfebcf0b542b5a12c3fc8b6b66347c98d20d
myschoolstory <168579347+myschoolstory@users.noreply.github.com>
2025-01-13 08:28:49 -0500

    Create __init__.py

---
+++ middleware/__init__.py
@@ -0,0 +1,5 @@
+from .vector_store_middleware import VectorStoreMiddleware
+
+__all__ = ['VectorStoreMiddleware']
+# This code will allow other parts of the project to import the `VectorStoreMiddleware` class using the following import statement: 
+#from middleware import VectorStoreMiddleware
